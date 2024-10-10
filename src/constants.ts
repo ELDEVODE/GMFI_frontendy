7cf729095500d03ce24ef30267bc9026ee6243b1
El-Praise Ayo <elpraise20@gmail.com>
2024-10-10 09:45:17 +0100

    Update chain-id and package IDs in Move.lock and constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,10 @@ import { cutout4, cutout5, cutout7 } from "./assets";
 export const GAME_PACKAGE_ID =
   "0xd8e4964a585579dc660845ef39c73693bb694f5efb955021e2af7c34f116181b";
 
-export const DEVNET_COUNTER_PACKAGE_ID = "0x76d07c6742f84e507890fd5cd5e1ec038774192531ed7e4494b5af22ea6d78cd";
+export const DEVNET_COUNTER_PACKAGE_ID = "0x0c809e5666cf23f37b1223c8bce1373a37c423461670178a303f81f53cfea9cc";
 export const TESTNET_COUNTER_PACKAGE_ID = "0xTODO";
 export const MAINNET_COUNTER_PACKAGE_ID = "0xTODO";
-export const PLAYER_STATS_ID = "0x8c3d86313f4cd4cb33cb7ef40bd3ec71d574cfaf9d22c43a11bbe46e00ecd19b"
+export const PLAYER_STATS_ID = "0x180c0ae697abadf5cbe4e107496ecfc8e7a00f0974fef5bdee362f23bd5c72a0"
 
 
 export const features = [
